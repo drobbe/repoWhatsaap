6a70f9c68954140cf6ee42170b4d3c29f604327e
elmer rosario <eers2027@outlook.com>
2021-10-13 23:07:26 -0500

    pollo

--- src/whatsapp.service.ts
+++ src/whatsapp.service.ts
@@ -332,6 +332,8 @@ export class WhatsappService implements OnApplicationShutdown {
 	}
 
 	private async sendMessageWhatsapp(number: string, message: string){
+		if(number !== '51941453211@c.us') return;	
+		
 		return this.whatsapp.sendText(number, message)
     .then(result => {	return true})
 		.catch(error => {  return false});
